c1268649876e2a807c4c1ef9cf03a4f991c1c8f9
IvanenkoTanya <55230849+IvanenkoTanya@users.noreply.github.com>
2019-09-17 19:01:21 +0300

    Update ListActivity.kt

--- app/src/main/java/com/example/testapp/views/ListActivity.kt
+++ app/src/main/java/com/example/testapp/views/ListActivity.kt
@@ -3,22 +3,18 @@ package com.example.testapp.views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.testapp.R
 import com.example.testapp.models.Employee
 import com.example.testapp.utils.DatabaseHandler
 import kotlinx.android.synthetic.main.activity_list.*
-import kotlinx.android.synthetic.main.employee_item.view.*
 
 class ListActivity : AppCompatActivity(){
 
-    var db = DatabaseHandler(this)
+    var database = DatabaseHandler(this)
     lateinit var employees : ArrayList<Employee>
-    lateinit var adapter: employeeListAdapter
+    lateinit var adapter: EmployeeListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +22,7 @@ class ListActivity : AppCompatActivity(){
 
         list_recycler.layoutManager = LinearLayoutManager(this)
 
-        employees = db.readData()
+        employees = database.readData()
 
         if(employees.isNotEmpty()){
             empty_list_text.visibility = View.GONE
@@ -35,7 +31,7 @@ class ListActivity : AppCompatActivity(){
             empty_list_text.visibility = View.VISIBLE
         }
 
-        adapter = employeeListAdapter(employees)
+        adapter = EmployeeListAdapter(employees)
         list_recycler.adapter = adapter
 
         add_new_btn_list_activity.setOnClickListener {
@@ -46,35 +42,15 @@ class ListActivity : AppCompatActivity(){
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if(requestCode == 1){
-            db = DatabaseHandler(this)
-            adapter.update(db.readData())
+            database = DatabaseHandler(this)
+            employees = database.readData()
+            adapter.update(employees)
+            if(employees.isNotEmpty()){
+                empty_list_text.visibility = View.GONE
+            }
+            else{
+                empty_list_text.visibility = View.VISIBLE
+            }
         }
-    }
-}
-
-
-class employeeListAdapter(var employeeList: ArrayList<Employee>) : RecyclerView.Adapter<employeeListAdapter.ViewHolder>(){
-
-    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.employee_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val employee = employeeList[position]
-        holder.view.employee_name_text.text = employee.name
-        holder.view.employee_age_text.text = employee.age.toString()
-        holder.view.employee_phone_text.text = employee.phone
-        holder.view.employee_gender_text.text = employee.gender.toString()
-    }
-
-    override fun getItemCount(): Int = employeeList.size
-
-    fun update(modelList:ArrayList<Employee>){
-        employeeList = modelList
-        notifyDataSetChanged()
     }
 }
